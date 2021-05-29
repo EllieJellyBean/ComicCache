3a4f3aab8b805e495b217773f078e21661169549
EllieAzaveda <elliemiller66@icloud.com>
2021-05-29 16:50:34 -0600

    Add Router and Switch

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AllComicsDisplay from '../AllComicsDisplay/AllComicsDisplay';
 import SingleComic from '../SingleComic/SingleComic';
+import { Route, Switch } from 'react-router-dom';
 import { fetchAllComics } from '../../Utils/APICalls';
 import './App.css';
 
@@ -8,7 +9,9 @@ class App extends Component {
   constructor() {
     super()
       this.state = {
-        allComics: []
+        allComics: [],
+        featuredComic: [],
+        error: ''
       }
   }
 
@@ -16,18 +19,33 @@ class App extends Component {
     fetchAllComics()
       .then(comicsData => {
         (typeof comicsData === 'string') ?
-          this.setState({ error: comicsData }) :
-          this.setState({ allComics: comicsData.results.books })
+        this.setState({ error: comicsData }) :
+        this.setState({ allComics: comicsData.results.books })
       })
+      .catch(err => this.setState({ error: 'Something went wrong. Please try again later.'} ))
   }
 
   render() {
-    return (
-      <div className="App">
+    console.log(this.state.allComics)
 
-      </div>
+    return (
+      <>
+        <div className="App">
+          {this.state.error && <h3 className='error-msg'>{this.state.error}</h3>}
+          {!this.state.error &&
+            <Switch>
+              <Route exact path="/" render={() => {
+                <AllComicsDisplay
+                  comicsData={this.state.allComics}
+                />
+              }}
+              />
+            </Switch>
+        </div>
+      </>
     )
- }
+  }
+
 }
 
 export default App;
