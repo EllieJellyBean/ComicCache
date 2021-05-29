30b4981690a162903a44e5efe15d2937004feb8a
EllieAzaveda <elliemiller66@icloud.com>
2021-05-29 16:51:03 -0600

    Add comic cover image

--- src/Components/SingleComic/SingleComic.js
+++ src/Components/SingleComic/SingleComic.js
@@ -3,6 +3,7 @@ import React from 'react';
 const ComicCard = ({ comic }) => {
   return (
     <div className='comic-card'>
+      <img className='comic-cover' src={comic.book_image} alt={`${comic.title} poster`}/>
       <h2 className='title'>{comic.title}</h2>
     </div>
   )
