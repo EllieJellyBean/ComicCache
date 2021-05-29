a711b1cb3fae369cd2bfdb767e43e04f667ba0cb
Your Name <stoner.michann@gmail.com>
2021-05-29 15:36:32 -0600

    Create functional component for featured comic

---
+++ src/Components/FeaturedComic/FeaturedComic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FeaturedComic = ({ featuredComic }) => {
+  console.log(featuredComic);
+  return (
+    <aside className='featured-comic'>
+      <h1>Title</h1>
+    </aside>
+  )
+}
+
+export default FeaturedComic
