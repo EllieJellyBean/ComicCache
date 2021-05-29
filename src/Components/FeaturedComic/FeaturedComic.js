6bb9563308f7f559710bdc0af83ced7e06897e02
Your Name <stoner.michann@gmail.com>
2021-05-29 15:46:46 -0600

    Add author and title

--- src/Components/FeaturedComic/FeaturedComic.js
+++ src/Components/FeaturedComic/FeaturedComic.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const FeaturedComic = ({ featuredComic }) => {
-  const { book_image, title, contributor, description, buy_links } = featuredComic;
+  const { book_image, title, author, description, buy_links } = featuredComic;
   console.log(featuredComic);
   return (
     <aside className='featured-comic'>
       <h3>top pick</h3>
       <img className='featured-image' src={book_image} />
+      <div className='info-container'>
+        <p className='author'>{author}</p>
+        <p className='title'>{title}</p>
+      </div>
     </aside>
   )
 }
