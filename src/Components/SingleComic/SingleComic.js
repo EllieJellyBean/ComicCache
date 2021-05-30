2449b36641b29ee06594c85e7a69bc5d9966174a
EllieAzaveda <elliemiller66@icloud.com>
2021-05-30 14:00:24 -0600

    Move link to title and image only

--- src/Components/SingleComic/SingleComic.js
+++ src/Components/SingleComic/SingleComic.js
@@ -6,15 +6,15 @@ const SingleComic = ({ comic, addToList }) => {
   const { title, book_image, rank } = comic;
 
   return (
-    <Link id={comic.rank} to={`/comic-details/${comic.rank}`} >
     <div className='comic-card'>
+    <Link id={comic.rank} to={`/comic-details/${comic.rank}`} >
       <img className='comic-image' src={book_image} alt={`${title} poster`}/>
       <h2 className='title'>{title}</h2>
+    </Link>
       <button id={rank} onClick={addToList}>
       Add to reading list
       </button>
     </div>
-    </Link>
   )
 }
 
