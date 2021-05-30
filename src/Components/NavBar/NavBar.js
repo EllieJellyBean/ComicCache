006e6be57264d56a93e9653ccc4bf16de1225974
Your Name <stoner.michann@gmail.com>
2021-05-30 11:19:45 -0600

    Add link to reading list

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './NavBar.css'
+import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav>
       <h1>comic cache</h1>
-      <ul>reading list</ul>
+      <Link to={'/reading-list'}>
+        <ul>reading list</ul>
+      </Link>
       <ul>home</ul>
     </nav>
 
