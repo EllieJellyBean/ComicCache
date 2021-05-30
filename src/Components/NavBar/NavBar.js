15d0b93903ba72def97e89548a26a59fc4550e24
Your Name <stoner.michann@gmail.com>
2021-05-30 11:24:20 -0600

    Add links to reading list and back to home

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,13 +3,16 @@ import './NavBar.css';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  const linkStyle = {textDecoration: 'none', color: 'black'};
   return (
     <nav>
       <h1>comic cache</h1>
-      <Link to={'/reading-list'}>
+      <Link to={'/reading-list'} style={linkStyle}>
         <ul>reading list</ul>
       </Link>
-      <ul>home</ul>
+      <Link to='/' style={linkStyle}>
+        <ul>home</ul>
+      </Link>
     </nav>
 
   )
