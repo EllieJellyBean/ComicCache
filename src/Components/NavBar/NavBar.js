1ff24c2af6ecbaf27af5b6923a6408fec012b759
Your Name <stoner.michann@gmail.com>
2021-05-30 09:39:16 -0600

    Add list items

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,6 +4,8 @@ const NavBar = () => {
   return (
     <nav>
       <img className='logo' src='favicon.ico' alt='comic-cache-logo'/>
+      <ul>reading list</ul>
+      <ul>home</ul>
     </nav>
 
   )
