f17631279c266f637d329f8ceea80e929cdc6ff6
Your Name <stoner.michann@gmail.com>
2021-05-30 09:33:08 -0600

    Add nav element with logo image

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const NavBar = () => {
   return (
-    
+    <nav>
+      <img className='logo' src='favicon.ico' alt='comic-cache-logo'/>
+    </nav>
+
   )
 }
 
 
 
-export default NavBar
+export default NavBar;
