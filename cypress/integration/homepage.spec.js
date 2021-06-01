d7488946c410f683398950e41f9f077aca95ac0a
Your Name <stoner.michann@gmail.com>
2021-05-31 20:21:29 -0600

    Add test for featured comic

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -20,17 +20,22 @@ describe('Homepage', () => {
   it('should have a nav bar', () => {
     cy.get('nav').get('h1').contains('comic cache')
       .get('ul').contains('reading list')
-      .get('ul').contains('home')
-      
-  })
+      .get('ul').contains('home')  
+  });
 
   it('should display a grid of top ranking comics', () => {
     cy.get('.main-container').get('.comic-card').get('h2').contains('A Title')
       .get('.comic-card').get('h2').contains('Different Title')
-  })
+  });
 
   it('should have a button to add a single comic to reading list', () => {
-    cy.get('.comic-card').get('.add-button').contains('Add to reading list')
+    cy.get('.comic-card').get('.add-button').contains('Add to reading list').should('be.visible')
       .click()
-  })
-})
+  });
+
+  it('should display a featured comic', () => {
+    cy.get('aside').should('be.visible').contains('top pick')
+      .get('img').should('be.visible')
+      .get('h2').should('be.visible').contains('A Title')
+  });
+});
