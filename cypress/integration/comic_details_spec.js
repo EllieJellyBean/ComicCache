5d7a9617e13b7ce8d4ca959071cf5b3276486a02
EllieAzaveda <elliemiller66@icloud.com>
2021-06-01 09:33:16 -0600

    Add url test

--- cypress/integration/comic_details_spec.js
+++ cypress/integration/comic_details_spec.js
@@ -1,7 +1,7 @@
 describe('Show single comic details display of Comic Cache', () => {
 
   beforeEach(() => {
-    cy.fixture('comicsMockData.json')
+    cy.fixture('comicDetailsMockData.json')
       .then(mockData => {
         cy.intercept('GET', 'https://api.nytimes.com/svc/books/v3/lists/manga.json?api-key=EGWgT37pIzbpuPsACX0SnMd3wKAwXhD9', {
           statusCode: 201,
@@ -12,6 +12,8 @@ describe('Show single comic details display of Comic Cache', () => {
     cy.visit('http://localhost:3000/comic-details/2')
   });
 
-
+  it('Should be able to visit a single comic details page', () => {
+    cy.url().should('eq', 'http://localhost:3000/comic-details/2')
+  });
 
 });
