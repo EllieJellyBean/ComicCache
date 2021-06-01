c556fd7e27b0b63898e1596d7e3a679194b39694
EllieAzaveda <elliemiller66@icloud.com>
2021-06-01 09:45:17 -0600

    Fix spacingg to details display

--- src/Components/ComicDetails/ComicDetails.js
+++ src/Components/ComicDetails/ComicDetails.js
@@ -6,15 +6,15 @@ const ComicDetails = ({ rank, foundComic }) => {
 
   return (
     <article className='details-display'>
-    <div className='comic-details'>
-      <h2 data-cy='details-title' className='details-title'>{title}</h2>
-      <img className='details-image' src={book_image} alt={`comic cover for ${title}`}/>
-      <h4 data-cy='comic-author' className='comic-author'>{`Author: ${author}`}</h4>
-      <h4 data-cy='comic-contributor' className='comic-contributor'>{`Contributor: ${contributor}`}</h4>
-      <h4 data-cy='comic-publisher' className='comic-publisher'>{`Publisher: ${publisher}`}</h4>
-      <h4 data-cy='comic-description' className='description'>Comic Description:</h4>
-      <h4 data-cy='comic-description' className='description'>{description}</h4>
-    </div>
+      <div className='comic-details'>
+        <h2 data-cy='details-title' className='details-title'>{title}</h2>
+        <img className='details-image' src={book_image} alt={`comic cover for ${title}`}/>
+        <h4 data-cy='comic-author' className='comic-author'>{`Author: ${author}`}</h4>
+        <h4 data-cy='comic-contributor' className='comic-contributor'>{`Contributor: ${contributor}`}</h4>
+        <h4 data-cy='comic-publisher' className='comic-publisher'>{`Publisher: ${publisher}`}</h4>
+        <h4 data-cy='comic-description' className='description'>Comic Description:</h4>
+        <h4 data-cy='comic-description' className='description'>{description}</h4>
+      </div>
     </article>
   )
 }
