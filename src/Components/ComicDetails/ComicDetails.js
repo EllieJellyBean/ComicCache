c4a6b5d754cc9145962382ae591632289c56233d
EllieAzaveda <elliemiller66@icloud.com>
2021-06-01 18:03:34 -0600

    Add destructured props

--- src/Components/ComicDetails/ComicDetails.js
+++ src/Components/ComicDetails/ComicDetails.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './ComicDetails.css';
 
-const ComicDetails = ({ rank, foundComic }) => {
-  const {  author, book_image, contributor, description, publisher, title } = foundComic;
+const ComicDetails = (props) => {
+  const { rank, foundComic, addComicToReadingList } = props;
 
   return (
     <article className='details-display'>
       <div className='comic-details'>
-        <h2 data-cy='details-title' className='details-title'>{title}</h2>
-        <img className='details-image' src={book_image} alt={`comic cover for ${title}`}/>
-        <h4 data-cy='comic-author' className='comic-author'>{`Author: ${author}`}</h4>
-        <h4 data-cy='comic-contributor' className='comic-contributor'>{`Contributor: ${contributor}`}</h4>
-        <h4 data-cy='comic-publisher' className='comic-publisher'>{`Publisher: ${publisher}`}</h4>
+        <h2 data-cy='details-title' className='details-title'>{foundComic.title}</h2>
+        <img className='details-image' src={foundComic.book_image} alt={`comic cover for ${foundComic.title}`}/>
+        <h4 data-cy='comic-author' className='comic-author'>{`Author: ${foundComic.author}`}</h4>
+        <h4 data-cy='comic-contributor' className='comic-contributor'>{`Contributor: ${foundComic.contributor}`}</h4>
+        <h4 data-cy='comic-publisher' className='comic-publisher'>{`Publisher: ${foundComic.publisher}`}</h4>
         <h4 data-cy='comic-description' className='description'>Comic Description:</h4>
-        <h4 data-cy='comic-description' className='description'>{description}</h4>
+        <h4 data-cy='comic-description' className='description'>{foundComic.description}</h4>
       </div>
     </article>
   )
