fefb9d467129b55635b47e82cfff6ddbbb29cd5e
EllieAzaveda <elliemiller66@icloud.com>
2021-06-01 20:25:23 -0600

    Change component  from functional to class

--- src/Components/ComicDetails/ComicDetails.js
+++ src/Components/ComicDetails/ComicDetails.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { fetchAllComics } from '../../Utils/APICalls';
 import './ComicDetails.css';
 
-const ComicDetails = (props) => {
-  const { rank, foundComic, addComicToReadingList } = props;
+class ComicDetails extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        rank: props.rank,
+        comicsData: [],
+        error: '',
+        foundComic: {}
+      }
+  }
 
-  return (
-    <article className='details-display'>
-      <NavLink to='/'><button data-cy='home-button' className='home-button'>Back to Home</button></NavLink>
-      <div className='comic-details'>
-        <h2 data-cy='details-title' className='details-title'>{foundComic.title}</h2>
-        <img className='details-image' src={foundComic.book_image} alt={`comic cover for ${foundComic.title}`}/>
-        <h4 data-cy='comic-author' className='comic-author'>{`Author: ${foundComic.author}`}</h4>
-        <h4 data-cy='comic-contributor' className='comic-contributor'>{`Contributor: ${foundComic.contributor}`}</h4>
-        <h4 data-cy='comic-publisher' className='comic-publisher'>{`Publisher: ${foundComic.publisher}`}</h4>
-        <h4 data-cy='comic-description' className='description'>Comic Description:</h4>
-        <h4 data-cy='comic-description' className='description'>{foundComic.description}</h4>
-        <button id={rank} onClick={addComicToReadingList}> Add to reading list</button>
-      </div>
-    </article>
-  )
+  componentDidMount = () => {
+    fetchAllComics()
+      .then(comicsData => {
+        (typeof comicsData === 'string') ?
+          this.setState({ error: comicsData }) :
+          this.setState({ foundComic: comicsData.results.books[this.state.rank] })
+      })
+      .catch(err => this.setState({ error: 'Something went wrong. Please try again later.'} ))
+  }
+
+  render() {
+    const { title, book_image, author, contributor, publisher, description } = this.state.foundComic;
+
+    return (
+      <article className='details-display'>
+        <NavLink to='/'><button data-cy='home-button' className='home-button'>Back to Home</button></NavLink>
+        <div className='comic-details'>
+          <h2 data-cy='details-title' className='details-title'>{title}</h2>
+          <img className='details-image' src={book_image} alt={`comic cover for ${title}`}/>
+          <h4 data-cy='comic-author' className='comic-author'>{`Author: ${author}`}</h4>
+          <h4 data-cy='comic-contributor' className='comic-contributor'>{`Contributor: ${contributor}`}</h4>
+          <h4 data-cy='comic-publisher' className='comic-publisher'>{`Publisher: ${publisher}`}</h4>
+          <h4 data-cy='comic-description' className='description'>Comic Description:</h4>
+          <h4 data-cy='comic-description' className='description'>{description}</h4>
+          <button id={this.state.rank} onClick={this.props.addComicToReadingList}> Add to reading list</button>
+        </div>
+      </article>
+    )
+  }
 }
 
 export default ComicDetails;
