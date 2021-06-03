94e34057645868e0e5e05d909ab24654523e6471
Your Name <stoner.michann@gmail.com>
2021-06-03 14:37:46 -0600

    Write function to clean data before using it in App

---
+++ src/Utils/cleaningFunctions.js
@@ -0,0 +1,15 @@
+const filterComicData = comicData => {
+  const filteredComicData = comicData.results.books.map(comic => {
+    return {
+      'rank': comic.rank,
+      'author': comic.author,
+      'book_image': comic.book_image,
+      'description': comic.description,
+      'buy_links': comic.buy_links[0],
+      'publisher': comic.publisher
+    }
+  });
+  return filteredComicData;
+}
+
+export { filterComicData };
