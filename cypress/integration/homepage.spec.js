63757956ba6368b55d2ed7a7fd0828550800d7bb
Your Name <stoner.michann@gmail.com>
2021-06-03 17:52:22 -0600

    Update intercept to use mock data file

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -3,7 +3,11 @@ describe('Display Homepage', () => {
   beforeEach(() => {
     cy.fixture('comicsMockData.json')
       .then(mockData => {
-        cy.intercept('https://api.nytimes.com/svc/books/v3/lists/manga.json?api-key=EGWgT37pIzbpuPsACX0SnMd3wKAwXhD9', { mockData })
+        cy.intercept('https://api.nytimes.com/svc/books/v3/lists/manga.json?api-key=EGWgT37pIzbpuPsACX0SnMd3wKAwXhD9', {
+          statusCode: 201,
+          delay: 100,
+          body: mockData 
+        })
     //   results: { books: [
     //     {
     //       'title': 'A Title',
