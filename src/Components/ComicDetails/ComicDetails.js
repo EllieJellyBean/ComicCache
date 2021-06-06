c7228ed443d139a961f6d67367ab5224e9e09708
Your Name <stoner.michann@gmail.com>
2021-06-06 14:45:15 -0600

    Add method for buy link

--- src/Components/ComicDetails/ComicDetails.js
+++ src/Components/ComicDetails/ComicDetails.js
@@ -24,18 +24,24 @@ class ComicDetails extends Component {
       .catch(err => this.setState({ error: 'Something went wrong. Please try again later.'} ))
   }
 
+  handleClick = buyLink => {
+    window.open(buyLink)
+  }
+
   render() {
-    const { title, book_image, author, contributor, publisher, description } = this.state.foundComic;
+    const { title, book_image, author, contributor, publisher, description, buy_links } = this.state.foundComic;
 
     return (
       <article className='details-display'>
-        <NavLink to='/'><button data-cy='home-button' className='home-button'>Back to Home</button></NavLink>
+          <NavLink to='/'>
+            <i className="fas fa-arrow-alt-circle-left fa-3x"></i>
+          </NavLink>
         <div className='comic-details'>
           <img className='details-image' src={book_image} alt={`comic cover for ${title}`}/>
           <h4 data-cy='comic-author' className='comic-author'>{author}</h4>
           <h2 data-cy='details-title' className='details-title'>{title}</h2>
-          <h4 data-cy='comic-publisher' className='comic-publisher'>{`Published by ${publisher}`}</h4>
           <h4 data-cy='comic-description' className='description'>{description}</h4>
+          <button className='buy-it-button details-button' onClick={() => this.handleClick(buy_links[0].url)}>BUY IT</button>
         </div>
       </article>
     )
