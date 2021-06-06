dea9a464f60d544f23f3b2e26e77c0058b324009
EllieAzaveda <elliemiller66@icloud.com>
2021-06-06 15:36:35 -0600

    Fix install event listener

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,35 @@
-const cacheName = 'my-cache';
-const staticContent = [
+// using const threw errors -> Changed to let && errors resolved
+let staticCache = 'static-cache';
+let dynamicCache = `dynamicCache`;
+let fontCache = `fontCache`;
+let imageCache = `imageCache`;
+
+let staticContent = [
   '/index.html',
   '/index.js',
   '/newicon.png',
-]
+  '/logo.png',
+  '/manifest.json',
+  '/robots.txt'
+];
+
+// let imgContent = [
+// ]
 
 self.addEventListener('install', event => {
+  self.skipWaiting()
   console.log('installing service worker')
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(staticContent))
-  )
-  .then(() => self.skipWaiting())
-})
+    caches.open(staticCache).then(cache => {
+      cache.addAll(staticContent).then(
+        () => {
+          console.log(`${staticCache} has been updated`)
+        },
+        (err) => {
+          console.warn(`failed to update${staticCache}`)
+        }
+      )
+    })
 
-self.addEventListener('activate', event => {
-  console.log('activating');
-})
+  )
+});
