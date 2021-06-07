677efb29653b40c1a3116b290a0532b52a246064
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-07 14:15:02 -0600

    Add test to make sure a user can remove a comic from the list

--- cypress/integration/reading_list_spec.js
+++ cypress/integration/reading_list_spec.js
@@ -20,10 +20,18 @@ describe('Adding and removing comics from reading list', () => {
         .get('[data-cy=remove-from-list]>p').eq(0).should('contain', 'remove')
   });
 
-  it('Should should be able display added cards in the list', () => {
+  it('Should be able display added cards in the list', () => {
       cy.get('[data-cy=add-to-list]>i').eq(0).click()
         .get('[data-cy=readingList]>p').click()
         .get('[data-cy=comic]').should('contain', 'ONE-PUNCH MAN, VOL. 10')
+  });
+
+  it('Should be able to remove a card from the list', () => {
+      cy.get('[data-cy=add-to-list]>i').eq(0).click()
+        .get('[data-cy=remove-from-list]>i').eq(0).click()
+        .get('[data-cy=readingList]>p').click()
+        .get('[data-cy=comic]').should('not.exist')
+        .get('div').should('contain', 'No comics in reading list!')
   });
 
 });
