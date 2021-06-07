9975889b78b4e8583ff64032e094ef9873ec49ea
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-07 14:19:58 -0600

    Add test to make sure a user can remove the card from the list from the dash and from within the list

--- cypress/integration/reading_list_spec.js
+++ cypress/integration/reading_list_spec.js
@@ -26,10 +26,18 @@ describe('Adding and removing comics from reading list', () => {
         .get('[data-cy=comic]').should('contain', 'ONE-PUNCH MAN, VOL. 10')
   });
 
-  it('Should be able to remove a card from the list', () => {
+  it('Should be able to remove a card from the reading list on the dash', () => {
       cy.get('[data-cy=add-to-list]>i').eq(0).click()
         .get('[data-cy=remove-from-list]>i').eq(0).click()
         .get('[data-cy=readingList]>p').click()
+        .get('[data-cy=comic]').should('not.exist')
+        .get('div').should('contain', 'No comics in reading list!')
+  });
+
+  it('Should be able to remove a card from the reading list from within the list', () => {
+      cy.get('[data-cy=add-to-list]>i').eq(0).click()
+        .get('[data-cy=readingList]>p').click()
+        .get('[data-cy=remove-from-list]>i').eq(0).click()
         .get('[data-cy=comic]').should('not.exist')
         .get('div').should('contain', 'No comics in reading list!')
   });
