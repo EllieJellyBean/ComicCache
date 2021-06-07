007751fbf0a1daa8a0c5781646ecfc1c7f0f7dcf
Your Name <stoner.michann@gmail.com>
2021-06-07 16:39:37 -0600

    Adjust spacing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -33,8 +33,8 @@ class App extends Component {
       if (localStorage.getItem('readingList')) {
         this.setState({ readingList: JSON.parse(localStorage.getItem('readingList')) })
       }
-      window.addEventListener("resize", this.updateSize);
-      window.addEventListener("load", this.updateSize);
+      window.addEventListener('resize', this.updateSize)
+      window.addEventListener('load', this.updateSize)
   }
 
   render() {
@@ -46,12 +46,14 @@ class App extends Component {
         <p className='nav-text'>FEATURED COMIC</p>
       </div>
     </Link>
+
     return (
-      <main className="App">
+      <main className='App'>
       {this.state.isMobile ?
         <NavBar featuredComicButton={featuredComicButton} linkStyle={linkStyle}/>
        :<NavBar linkStyle={linkStyle}/>
       }
+
         <Switch>
         {!this.state.allComics.length && !this.state.error && <h2>Loading Content...</h2>}
         {this.state.error && <h2>{this.state.error}</h2>}
@@ -66,7 +68,6 @@ class App extends Component {
               {!this.state.isMobile &&
                 <FeaturedComic featuredComic={this.state.featuredComic}/>
               }
-
             </div>
           )}
         />
@@ -96,7 +97,7 @@ class App extends Component {
             </div>
           )}
         />
-        <Route path="/comic-details/:rank" render={({ match }) => {
+        <Route path='/comic-details/:rank' render={({ match }) => {
           const { rank } = match.params;
 
           return <ComicDetails
@@ -114,14 +115,16 @@ class App extends Component {
   addComicToReadingList = (event) => {
      const readingList = this.state.readingList
      const comicToAdd = this.state.allComics.find( comic => comic.rank === parseInt(event.target.id))
-       if (!readingList.includes(comicToAdd)) {
+
+      if (!readingList.includes(comicToAdd)) {
         this.setState({readingList: [...readingList, comicToAdd]})
         setTimeout(this.setLocalStorage, 50)
+      }
   }
-}
 
   removeComicFromReadingList = (event) => {
      const filteredList = this.state.readingList.filter(comic => comic.rank !== parseInt(event.target.id));
+
      this.setState({ readingList: filteredList });
      setTimeout(this.setLocalStorage, 50)
    }
