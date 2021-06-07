f11414047e5dba95c4952abd99f9c4342df3d7f7
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-07 13:32:37 -0600

    Add stub

--- cypress/integration/reading_list_spec.js
+++ cypress/integration/reading_list_spec.js
@@ -1,6 +1,16 @@
 describe('Adding and removing comics from reading list', () => {
 
   beforeEach(() => {
+    beforeEach(() => {
+      cy.fixture('comicsMockData.json')
+        .then(mockData => {
+        .intercept('https://api.nytimes.com/svc/books/v3/lists/manga.json?api-key=EGWgT37pIzbpuPsACX0SnMd3wKAwXhD9', {
+            statusCode: 201,
+            delay: 100,
+            body: mockData
+          })
+        })
+    })
   })
 
 });
