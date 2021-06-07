a7597ca475fa6261bd89b3fe4aa0d2dc6f81558d
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-07 14:32:32 -0600

    Implement custom cypress command

--- cypress/integration/reading_list_spec.js
+++ cypress/integration/reading_list_spec.js
@@ -1,14 +1,7 @@
 describe('Adding and removing comics from reading list', () => {
 
   beforeEach(() => {
-    cy.fixture('comicsMockData.json')
-      .then(mockData => {
-        cy.intercept('https://api.nytimes.com/svc/books/v3/lists/manga.json?api-key=EGWgT37pIzbpuPsACX0SnMd3wKAwXhD9', {
-          statusCode: 201,
-          delay: 100,
-          body: mockData
-        })
-      })
+    cy.stub()
       .visit('http://localhost:3000/')
   })
 
